41514308a86a81032e665c4fe518c0331c2d053c
Per Dervall <pdervall@gmail.com>
2012-01-23 21:34:47 +0100

    Created testcase for escaping +

---
+++ Tests/TestRegEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Piglet.Lexer;
+
+namespace TestParser
+{
+    [TestClass]
+    public class TestRegEx
+    {
+        ILexer<string> CreateLexer(string regEx)
+        {
+            return LexerFactory<string>.Configure(c => c.Token(regEx, f => regEx));
+        }
+            
+        [TestMethod]
+        public void TestEscapedCharacters()
+        {
+            ILexer<string> lexer = CreateLexer("\\++");
+            lexer.Source = new StringReader("++++");
+            Assert.AreEqual("\\++", lexer.Next().Item2);
+        }
+    }
+}

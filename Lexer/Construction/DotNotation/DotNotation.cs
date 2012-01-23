1ddc9a55a3876fae3d7773c39f859fcd7094beb4
Per Dervall <pdervall@gmail.com>
2012-01-23 17:27:39 +0100

    Dot notation extension method created

---
+++ Lexer/Construction/DotNotation/DotNotation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Piglet.Lexer.Construction.DotNotation
+{
+    public static class DotNotation
+    {
+        /// <summary>
+        /// Print the state machine as DOT notation suitable for drawing graphs.
+        /// This is a useful debug functionality
+        /// </summary>
+        /// <param name="nfa"></param>
+        /// <returns></returns>
+        public static string AsDotNotation(this NFA nfa)
+        {
+            return "";
+        }
+    }
+}

c577f268d61671eb7056697c1168ee3983bd92ae
Per Dervall <pdervall@gmail.com>
2012-01-23 19:26:59 +0100

    Added dotnottation

--- Lexer/Construction/DotNotation/DotNotation.cs
+++ Lexer/Construction/DotNotation/DotNotation.cs
@@ -10,12 +10,58 @@ namespace Piglet.Lexer.Construction.DotNotation
         /// <summary>
         /// Print the state machine as DOT notation suitable for drawing graphs.
         /// This is a useful debug functionality
+        /// 
+        /// http://hughesbennett.co.uk/Graphviz copy and paste your text to view graph
+        /// 
         /// </summary>
-        /// <param name="nfa"></param>
+        /// <param name="automata"></param>
+        /// <param name="graphName"></param>
         /// <returns></returns>
-        public static string AsDotNotation(this NFA nfa)
+        public static string AsDotNotation(this NFA automata, string graphName = "automata")
         {
-            return "";
+            // Draw the *FA as a directed graph with the state numbers in circles
+            // Use a double circle for accepting states
+            //
+            // digraph graphname {
+            // digraph G {
+            //   [node shape="circle"]
+            //   1 [shape="doublecircle"]
+            //  1 -> 2 [label=a]
+            //}
+
+            var sb = new StringBuilder();
+
+            sb.Append("digraph " + graphName + " {\n");
+            sb.Append("\t[node shape=\"circle\"]\n");
+
+            foreach (var state in automata.States.Where(f=>f.AcceptState))
+            {
+                sb.Append(string.Format("{0} [shape=\"doublecircle\"]\n", state.StateNumber));
+            }
+            
+            foreach (var transition in automata.Transitions)
+            {
+                
+                sb.Append(string.Format("\t{0} -> {1} [label={2}]\n", 
+                    transition.From.StateNumber,
+                    transition.To.StateNumber,
+                    transition.TransitionLabel() ));
+            }
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+
+        /// <summary>
+        /// DOT language label name for transitions
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="transition"></param>
+        /// <returns></returns>
+        public static string TransitionLabel<T>(this Transition<T> transition )
+        {
+            return transition.OnCharacter == (char)0 ? "ε" : "" + transition.OnCharacter;
         }
     }
 }

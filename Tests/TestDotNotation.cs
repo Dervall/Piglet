0d02111546a6aee390c420feeb3a2660b0f5e0b5
Per Dervall <pdervall@gmail.com>
2012-01-23 19:28:32 +0100

    Added basic tests for DOT notation graphs

---
+++ Tests/TestDotNotation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Piglet.Lexer.Construction;
+using Piglet.Lexer.Construction.DotNotation;
+
+namespace TestParser
+{
+    [TestClass]
+    public class TestDotNotation
+    {
+        [TestMethod]
+        public void TestDotForNFA()
+        {
+            var nfa = NFA.Create(PostFixConverter.ToPostFix("((hej)|(tjo))+hopp"));
+            string dotString = nfa.AsDotNotation();
+            Console.WriteLine(dotString);
+            Console.WriteLine();
+        }
+    }
+}

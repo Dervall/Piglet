d4eea51595e09dda4846c92dd77bc720a198601e
unknown6656 <unknown84@gmx.de>
2020-06-10 22:35:32 +0200

    Continued adding XML doc comments.

--- Piglet.GenericGenerator/Program.cs
+++ Piglet.GenericGenerator/Program.cs
@@ -81,46 +81,106 @@ using Piglet.Lexer.Runtime;
 
 namespace Piglet.Parser.Configuration.Generic
 {{
+    /// <summary>
+    /// Represents an abstract generic parser constructor.
+    /// <para/>
+    /// The parser based on this constructor will return a parsed value of the type <typeparamref name=""TOut""/>.
+    /// </summary>
+    /// <typeparam name=""TOut"">The generic value return type of the parser.</typeparam>
     public abstract class ParserConstructor<TOut>
     {{
         private ParserWrapper? _parser = null;
         private volatile int _ntcounter = 0;
 
+        /// <summary>
+        /// The parser configuration.
+        /// </summary>
         public IParserConfigurator<object> Configurator {{ get; }}
 
 
+        /// <summary>
+        /// Creates a new generic parser constructor with the default parser configuration.
+        /// </summary>
         public ParserConstructor()
             : this(ParserFactory.Configure<object>())
         {{
         }}
 
+        /// <summary>
+        /// Creates a new generic parser constructor with the given parser configuration.
+        /// </summary>
+        /// <param name=""configurator"">Parser configuration.</param>
         public ParserConstructor(IParserConfigurator<object> configurator) => Configurator = configurator;
 
+        /// <summary>
+        /// Creates a new non-terminal symbol with the given generic semantic value and name.
+        /// </summary>
+        /// <typeparam name=""T"">Generic semantic value stored inside the new non-terminal symbol.</typeparam>
+        /// <param name=""name"">The name of the new non-terminal symbol.</param>
+        /// <returns>The newly created non-terminal symbol.</returns>
         protected NonTerminalWrapper<T> CreateNonTerminal<T>(string name) => new NonTerminalWrapper<T>(Configurator.CreateNonTerminal(name));
 
+        /// <summary>
+        /// Creates a new non-terminal symbol with the given generic semantic value and the default name for non-terminals (""NT..."").
+        /// </summary>
+        /// <typeparam name=""T"">Generic semantic value stored inside the new non-terminal symbol.</typeparam>
+        /// <returns>The newly created non-terminal symbol.</returns>
         protected NonTerminalWrapper<T> CreateNonTerminal<T>() => CreateNonTerminal<T>($""NT{{++_ntcounter}}"");
 
+        /// <summary>
+        /// Creates a new terminal symbol associated with the given regex string and generic value.
+        /// </summary>
+        /// <typeparam name=""T"">Generic semantic value stored inside the new terminal symbol.</typeparam>
+        /// <param name=""regex"">The regex string associated with the terminal symbol.</param>
+        /// <param name=""value"">The value stored inside the new terminal value.</param>
+        /// <returns>The newly created terminal symbol.</returns>
+        protected TerminalWrapper<T> CreateTerminal<T>(string regex, T value) => CreateTerminal(regex, _ => value);
+
+        /// <summary>
+        /// Creates a new terminal symbol associated with the given regex string and the function providing the generic value.
+        /// </summary>
+        /// <typeparam name=""T"">Generic semantic value stored inside the new terminal symbol.</typeparam>
+        /// <param name=""regex"">The regex string associated with the terminal symbol.</param>
+        /// <param name=""func"">The function providing the generic value represented by the terminal symbol.</param>
+        /// <returns>The newly created terminal symbol.</returns>
         protected TerminalWrapper<T> CreateTerminal<T>(string regex, Func<string, T> func) => new TerminalWrapper<T>(Configurator.CreateTerminal(regex, s => func(s)));
 
+        /// <summary>
+        /// Creates a new terminal symbol associated with the given regex string and the identity function (of the type <see langword=""string""/>).
+        /// </summary>
+        /// <param name=""regex"">The regex string associated with the terminal symbol.</param>
+        /// <returns>The newly created terminal symbol.</returns>
         protected TerminalWrapper<string> CreateTerminal(string regex) => new TerminalWrapper<string>(Configurator.CreateTerminal(regex));
 
-        /// <summary>ascending precedence!</summary>
-        protected void SetPrecedenceList(params (AssociativityDirection direction, ITerminalWrapper[] symbols)[] group)
+        /// <summary>
+        /// Sets the precedence for all given symbols in ascending order. The first symbol group is therefore considered to have the lowest precedence and the last symbol group the highest precedence.
+        /// </summary>
+        /// <param name=""groups"">Ordered collection of groups containing a set of symbols with their corresponding associativity.</param>
+        protected void SetPrecedenceList(params (AssociativityDirection direction, ITerminalWrapper[] symbols)[] groups)
         {{
-            foreach ((AssociativityDirection d, ITerminalWrapper[] s) in group)
+            foreach ((AssociativityDirection d, ITerminalWrapper[] s) in groups)
                 SetAssociativity(d, s);
         }}
 
-        protected IPrecedenceGroup SetAssociativity(AssociativityDirection dir, params ITerminalWrapper[] symbol)
+        /// <summary>
+        /// Sets the given associativity to all symbols in the given symbol collection. All sybols will be considered to have the same precedence group.
+        /// </summary>
+        /// <param name=""dir"">Associativity direction.</param>
+        /// <param name=""symbols"">Target symbols.</param>
+        /// <returns>The precedence group associated with the given symbols.</returns>
+        protected IPrecedenceGroup SetAssociativity(AssociativityDirection dir, params ITerminalWrapper[] symbols)
         {{
-            ITerminal<object>[] arr = symbol.Select(s => s.Symbol).ToArray();
+            ITerminal<object>[] arr = symbols.Select(s => s.Symbol).ToArray();
 
-            if (dir == AssociativityDirection.Left)
-                return Configurator.LeftAssociative(arr);
-            else if (dir == AssociativityDirection.Right)
-                return Configurator.RightAssociative(arr);
-            else
-                throw new ArgumentException();
+            switch (dir)
+            {{
+                case AssociativityDirection.Left:
+                    return Configurator.LeftAssociative(arr);
+                case AssociativityDirection.Right:
+                    return Configurator.RightAssociative(arr);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(dir));
+            }}
         }}
 
         protected void CreateProduction<T, S0>(NonTerminalWrapper<T> to, SymbolWrapper<S0> s) => to.AddProduction(s).SetReduceToFirst();
@@ -138,103 +198,118 @@ namespace Piglet.Parser.Configuration.Generic
 ");
             }
 
-            writer.Write($@"
+            writer.Write(@"
+        /// <summary>
+        /// Creates a new parser based on the current configurator and returns it.
+        /// <para/>
+        /// <b>Note:</b> Only the first call of <see cref=""CreateParser""/> creates a new parser. If you wish to reset the generated parser and re-create it, call the method <see cref=""ReesetParser""/> beforehand.
+        /// </summary>
+        /// <returns>The constructed parser.</returns>
         public ParserWrapper CreateParser()
-        {{
+        {
             if (_parser is null)
-            {{
+            {
                 NonTerminalWrapper<TOut> nt = CreateNonTerminal<TOut>();
 
                 Construct(nt);
 
                 Configurator.SetStartSymbol((INonTerminal<object>)nt.Symbol);
                 _parser = new ParserWrapper(Configurator.CreateParser());
-            }}
+            }
 
             return _parser;
-        }}
+        }
 
+        /// <summary>
+        /// Resets the constructed parser to <see langword=""null""/>, thereby forcing the parser to be re-constructed based on the current conficuration the next time <see cref=""CreateParser""/> is called.
+        /// </summary>
+        public void ResetParser() => _parser = null;
+
+        /// <summary>
+        /// Constructs the parser. This method must be implemented by every constructor based on <see cref=""ParserConstructor{TOut}""/>
+        /// </summary>
+        /// <param name=""start_symbol"">The non-terminal production start symbol. The value of this symbol will be returned when the constructed parser is executed.</param>
         protected abstract void Construct(NonTerminalWrapper<TOut> start_symbol);
 
 
         public sealed class ParserWrapper
-        {{
-            public IParser<object> Parser {{ get; }}
+        {
+            public IParser<object> Parser { get; }
 
 
             internal ParserWrapper(IParser<object> parser) => Parser = parser;
 
             public ParserResult<TOut> Parse(string s)
-            {{
+            {
                 List<LexedToken<object>> tokens = new List<LexedToken<object>>();
                 void aggregate(LexedToken<object> token)
-                {{
+                {
                     tokens.Add(token);
 
                     if (token is LexedNonTerminal<object> nt)
                         foreach (LexedToken<object> child in nt.ChildNodes)
                             aggregate(child);
-                }}
+                }
 
                 aggregate(Parser.ParseTokens(s));
 
                 return new ParserResult<TOut>((TOut)tokens[0].SymbolValue, s.Split('\n'), tokens.ToArray());
-            }}
-        }}
-    }}
+            }
+        }
+    }
 
     public sealed class ParserResult<TOut>
-    {{
-        public TOut ParsedValue {{ get; }}
-        public string[] SourceLines {{ get; }}
-        public LexedToken<object>[] LexedTokens {{ get; }}
+    {
+        public TOut ParsedValue { get; }
+        public string[] SourceLines { get; }
+        public LexedToken<object>[] LexedTokens { get; }
 
 
         internal ParserResult(TOut parsedValue, string[] sourceLines, LexedToken<object>[] lexedTokens)
-        {{
+        {
             ParsedValue = parsedValue;
             SourceLines = sourceLines;
             LexedTokens = lexedTokens;
-        }}
+        }
 
         public static implicit operator TOut(ParserResult<TOut> res) => res.ParsedValue;
-    }}
+    }
 
     public interface ITerminalWrapper
-    {{
-        ITerminal<object> Symbol {{ get; }}
-    }}
+    {
+        ITerminal<object> Symbol { get; }
+    }
 
     public class SymbolWrapper<T>
-    {{
-        public ISymbol<object> Symbol {{ get; }}
+    {
+        public ISymbol<object> Symbol { get; }
         public Type SymbolType => typeof(T);
 
 
         public SymbolWrapper(ISymbol<object> symbol) => Symbol = symbol;
 
         public override string ToString() => Symbol.ToString();
-    }}
+    }
 
     public sealed class TerminalWrapper<T>
         : SymbolWrapper<T>
         , ITerminalWrapper
-    {{
+    {
         ITerminal<object> ITerminalWrapper.Symbol => (ITerminal<object>)Symbol;
 
         public TerminalWrapper(ISymbol<object> symbol)
             : base(symbol)
-        {{
-        }}
-    }}
+        {
+        }
+    }
 
     public sealed class NonTerminalWrapper<T>
         : SymbolWrapper<T>
-    {{
+    {
         public NonTerminalWrapper(ISymbol<object> symbol)
             : base(symbol)
-        {{
-        }}
+        {
+        }
 
         public ProductionWrapper<T> AddProduction() => new ProductionWrapper<T>(((INonTerminal<object>)Symbol).AddProduction());
 ");
